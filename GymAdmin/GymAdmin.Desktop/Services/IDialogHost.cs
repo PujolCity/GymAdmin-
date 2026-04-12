d42b11a3171ab6005b6fe447925c8ddef091fde6
PujolCity <53952983+PujolCity@users.noreply.github.com>
2026-04-12 14:40:53 -0300

    Se agrega la restauracion de los Backups diarios
    - Se abstrae el dialog de confirmacion para no repetir codigo (falta implementarlo en algunos ViewModels)
    -

---
+++ GymAdmin/GymAdmin.Desktop/Services/IDialogHost.cs
@@ -0,0 +1,7 @@
+﻿namespace GymAdmin.Desktop.Services;
+
+public interface IDialogHost
+{
+    object? DialogContent { get; set; }
+    bool IsDialogOpen { get; set; }
+}
---
+++ GymAdmin/GymAdmin.Desktop/Services/OverlayDialogService.cs
@@ -0,0 +1,51 @@
+﻿using GymAdmin.Desktop.ViewModels.Dialogs;
+using GymAdmin.Desktop.Views.Dialogs;
+using System.Windows;
+
+namespace GymAdmin.Desktop.Services;
+
+public class OverlayDialogService : IOverlayDialogService
+{
+    public async Task<bool> ShowConfirmAsync(
+         IDialogHost host,
+         string title,
+         string message,
+         string accept = "Aceptar",
+         string cancel = "Cancelar",
+         string acceptStyleKey = "DangerButtonStyle",
+         string cancelStyleKey = "PrimaryButtonStyle",
+         bool showCancel = true)
+    {
+        if (host is null)
+            throw new ArgumentNullException(nameof(host));
+
+        var vm = new ConfirmDialogViewModel
+        {
+            Title = title,
+            Message = message,
+            AcceptText = accept,
+            CancelText = cancel,
+            AcceptButtonStyle = (Style)Application.Current.FindResource(acceptStyleKey),
+            CancelButtonStyle = (Style)Application.Current.FindResource(cancelStyleKey),
+            ShowCancelButton = showCancel
+        };
+
+        var view = new ConfirmDialogView
+        {
+            DataContext = vm
+        };
+
+        host.DialogContent = view;
+        host.IsDialogOpen = true;
+
+        try
+        {
+            return await vm.Task;
+        }
+        finally
+        {
+            host.IsDialogOpen = false;
+            host.DialogContent = null;
+        }
+    }
+}
